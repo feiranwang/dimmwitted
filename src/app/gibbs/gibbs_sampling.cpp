4e34805eed7075dc879fc38823135b8aa3a32c87
zhangce <czhang@cs.wisc.edu>
2014-10-28 14:58:28 -0500

    Update gibbs_sampling.cpp

--- src/app/gibbs/gibbs_sampling.cpp
+++ src/app/gibbs/gibbs_sampling.cpp
@@ -17,8 +17,8 @@
  */
 void dd::GibbsSampling::prepare(){
 
-  //n_numa_nodes = numa_max_node();
-  n_numa_nodes = 0;
+  n_numa_nodes = numa_max_node();
+  //n_numa_nodes = 0;
   n_thread_per_numa = (sysconf(_SC_NPROCESSORS_CONF))/(n_numa_nodes+1);
   //n_thread_per_numa /= 2;
   //if(n_thread_per_numa == 0){
